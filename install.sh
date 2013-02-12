cd445dc552044666d948994c81145826afd581d2
Jim Posen <jim.posen@gmail.com>
2013-02-12 18:42:55 -0500

    Using native emacs24 package.

--- install.sh
+++ install.sh
@@ -1,9 +1,3 @@
 #!/bin/sh
 
-# emacs 24
-add-apt-repository ppa:cassou/emacs
-apt-get update
-apt-get install emacs24 emacs24-el emacs24-common-non-dfsg
-
-# zsh, git, curl
-apt-get install zsh git-core, curl
+apt-get install emacs24 zsh git-core curl
