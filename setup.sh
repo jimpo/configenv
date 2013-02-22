a960cb4e1064f3a7257d20f46852573fb6fd3a48
Jim Posen <jim.posen@gmail.com>
2013-02-22 11:59:33 -0500

    Change shell to ZSH in setup.sh.

--- setup.sh
+++ setup.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
 
 # install Oh My Zsh
-if [ ! -e $HOME/.oh-my-zsh ] ; then
+if [ ! -d $HOME/.oh-my-zsh ] ; then
     curl -L https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | sh
 fi
+
+# change default shell to ZSH
+chsh -s $(which zsh)
 
 # add customization files
 mv -i screenrc ~/.screenrc
