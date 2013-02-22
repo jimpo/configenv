6166e514993cf5778ed2ddeca5f7be0af54f97f8
Jim Posen <jim.posen@gmail.com>
2013-02-22 12:13:41 -0500

    Added custom zsh configuration files.

---
+++ oh-my-zsh/custom/aliases.zsh
@@ -0,0 +1,11 @@
+# some more ls aliases
+alias ll='ls -alF'
+alias la='ls -A'
+alias l='ls -CF'
+
+alias emacs='emacs -nw'
+
+alias ....='cd ../../..'
+alias .....='cd ../../../..'
+alias ......='cd ../../../../..'
+alias .......='cd ../../../../../..'
---
+++ oh-my-zsh/custom/settings.zsh
@@ -0,0 +1,10 @@
+autoload -U select-word-style
+select-word-style bash
+
+if [ -f ~/.rvm/scripts/rvm ] ; then
+   . ~/.rvm/scripts/rvm
+fi
+
+if [ -f ~/.nvm/nvm.sh ] ; then
+   . ~/.nvm/nvm.sh
+fi
