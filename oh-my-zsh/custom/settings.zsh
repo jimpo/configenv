55b65f9b4c96bf568abc9b4ec5fe82f8f98a6a2f
Jim Posen <jimpo@coinbase.com>
2018-05-04 17:53:49 -0700

    Modify settings.zsh.

--- oh-my-zsh/custom/settings.zsh
+++ oh-my-zsh/custom/settings.zsh
@@ -1,10 +1,16 @@
 autoload -U select-word-style
 select-word-style bash
 
-export GOPATH="$HOME/Code/golang"
+export EDITOR=vim
 
-export PATH="$PATH:$HOME/.rbenv/bin:$GOPATH/bin"
+export PATH="$PATH:$HOME/.rbenv/bin"
 eval "$(rbenv init -)"
 
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
+
+export GOPATH="$HOME/Code/golang"
+export PATH="$PATH:$GOPATH/bin"
+
+# pipenv
+export PATH="$PATH:$HOME/.local/bin"
