abaa04f8f2e78fa2ab78e5377ed6e3611f6d91b3
Jim Posen <jim.posen@gmail.com>
2026-07-28 12:55:34 +0200

    Remove setup-xkb from setup

--- setup.sh
+++ setup.sh
@@ -10,10 +10,3 @@ cp -ir jj ~/.config
 
 git clone https://github.com/chriskempson/base16-shell.git ~/.config/base16-shell
 
-# Capslock as meta remapping option
-cd /usr/share/X11/xkb/rules
-sudo sed -i 's/^  caps:menu\t\t=\t+capslock(menu)$/  caps:meta\t\t=\t+capslock(meta)\n\0/' base
-sudo sed -i 's/^  caps:menu\( \+\)Make Caps Lock an additional Menu key$/  caps:meta\1Make Caps Lock an additional Meta key\n\0/' base.lst
-sudo sed -i 's/^  caps:menu\t\t=\t+capslock(menu)$/  caps:meta\t\t=\t+capslock(meta)\n\0/' evdev
-sudo sed -i 's/^  caps:menu\( \+\)Make Caps Lock an additional Menu key$/  caps:meta\1Make Caps Lock an additional Meta key\n\0/' evdev.lst
-
