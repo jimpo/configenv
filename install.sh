4ad238f5dc0f8de93b20653ad7cc2e3b272adfd0
Jim Posen <jim.posen@gmail.com>
2026-07-28 12:21:06 +0200

    Update gitconfig

--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
-apt-get install emacs vim zsh git curl tmux zip unzip fonts-inconsolata direnv fzf ripgrep
+apt-get install emacs vim zsh git curl tmux zip unzip fonts-inconsolata direnv fzf ripgrep git-delta
+
